ed2e06581541f70980851744bb225aaa1547127e
Viktor <viktor.originaly@gmail.com>
2018-08-17 16:19:27 +0300

    Swipe View animated and Resume Button returns Swipe View with animation

--- SwipeToPauseAnimation/ViewController.swift
+++ SwipeToPauseAnimation/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftBorderConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rightBorderConstraint: NSLayoutConstraint!
+    @IBAction func showSwipeButton(_ sender: UIButton) {
+        self.leftBorderConstraint.constant = 0
+        self.rightBorderConstraint.constant = 375
+        UIView.animate(withDuration: 1.0,
+                       delay: 0.0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 1,
+                       options: .curveEaseInOut,
+                       animations: {
+                        self.view.layoutIfNeeded()
+                        
+        }, completion: nil)
+    }
+    
+    @IBAction func swipeGesture(_ sender: UISwipeGestureRecognizer) {
+        self.leftBorderConstraint.constant = 375
+        self.rightBorderConstraint.constant = -375
+        UIView.animate(withDuration: 1.0) {
+                        self.view.layoutIfNeeded()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
